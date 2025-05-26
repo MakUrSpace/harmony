c182876ccb207c58f33bd7f0aa22f2a1fed927e9
musingsole <musingsole@gmail.com>
2025-05-25 21:40:27 -0400

    Don't allow destroyed object actions

--- harmony/harmonyServer.py
+++ harmony/harmonyServer.py
@@ -435,7 +435,7 @@ def captureToActionRow(capture):
     objectVisual = imageToBase64(app.cm.object_visual(capture, withContours=False))
 
     objectActions = []
-    if app.cm.object_type(capture.oid) == "Unit":
+    if app.cm.object_type(capture.oid) == "Unit" and not app.cm.obj_destroyed(capture.oid):
         visibilityMap = f"""<img class="img-fluid border border-info border-2" alt="Visibility Map" src="data:image/jpg;base64,{imageToBase64(app.cm.buildVisibilityMap(capture))}" style="border-radius: 10px;">"""
         try:
             capDeclaredAction = app.cm.GameEvents.get_existing_declarations(capture.oid)[0]
@@ -517,6 +517,8 @@ def buildObjectTable(faction_filter=None, type_filter=None):
         rowRenderer = captureToChangeRow if gamePhase in ["Add", "Move"] else captureToActionRow
         for capture in app.cm.memory:
             print(f"Cap {capture.oid} -- Faction: {app.cm.faction(capture.oid)} -- Type: {app.cm.object_type(capture.oid)}")
+            if gamePhase == "Declare" and app.cm.is_destroyed(capture.oid):
+                continue
             if ((faction_filter is not None and faction_filter != "All" and app.cm.faction(capture.oid) != faction_filter)
                 or (type_filter is not None and type_filter != "All" and app.cm.object_type(capture.oid) != type_filter)
             ):
