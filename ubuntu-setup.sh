306874f4284c9ae91ee1701672c08c2f34c4f7b0
musingsole <musingsole@gmail.com>
2026-01-18 01:26:27 +0000

    Remove old packaging files

--- ubuntu-setup.sh
+++
@@ -1,7 +0,0 @@
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon --yes
-curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
-  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
-  | sudo tee /etc/apt/sources.list.d/ngrok.list \
-  && sudo apt update \
-  && sudo apt install -y ngrok tmux
