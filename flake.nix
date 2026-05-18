67918bd1d50472e97c1c4efa4d1329ad541e96e7
musingsole <musingsole@gmail.com>
2026-05-17 23:29:19 -0400

    ngrok http2 fix

--- flake.nix
+++ flake.nix
@@ -131,11 +131,11 @@
             
             # Split for Harmony Ngrok (horizontal split)
             tmux split-window -t "$SESSION_NAME:0" -h \
-              "echo 'Exposing Harmony on $HARMONY_URL -> $PORT'; ngrok http $PORT --domain=$HARMONY_URL --app-protocol=h2; read"
+              "echo 'Exposing Harmony on $HARMONY_URL -> $PORT'; ngrok http $PORT --domain=$HARMONY_URL --upstream-protocol=http2; read"
             
             # Split for Admin Ngrok (vertical split of the new pane)
             tmux split-window -t "$SESSION_NAME:0.1" -v \
-              "echo 'Exposing Admin on $ADMIN_URL -> $ADMIN_PORT'; ngrok http $ADMIN_PORT --domain=$ADMIN_URL --app-protocol=h2; read"
+              "echo 'Exposing Admin on $ADMIN_URL -> $ADMIN_PORT'; ngrok http $ADMIN_PORT --domain=$ADMIN_URL --upstream-protocol=http2; read"
             
             # Reorganize tiles
             tmux select-layout -t "$SESSION_NAME:0" tiled
